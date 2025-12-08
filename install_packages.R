93045318ccf786aecf3a8c82ed78d45e68c72f09
kxvlar <keval.amin@berkeley.edu>
2025-12-07 17:00:25 -0800

    renv resetup manually

---
+++ install_packages.R
@@ -0,0 +1,12 @@
+# install_packages.R
+# Install all R packages needed for Stat 156 project
+
+packages <- c(
+  "car",
+  "Matching",
+  "rdrobust",
+  "rdd",
+  "MatchIt"
+)
+
+install.packages(packages)
