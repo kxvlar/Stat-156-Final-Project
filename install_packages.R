7d258984d0bfdedb5f3e89f64ea95f897ad89158
kxvlar <keval.amin@berkeley.edu>
2025-12-09 03:14:36 -0800

    first attempt at loading data

--- install_packages.R
+++ install_packages.R
@@ -2,11 +2,13 @@
 # Install all R packages needed for Stat 156 project (add as we need)
 
 packages <- c(
+  "dplyr",
   "car",
   "Matching",
   "rdrobust",
   "rdd",
-  "MatchIt"
+  "MatchIt",
+  "haven"
 )
 
 install.packages(packages)
